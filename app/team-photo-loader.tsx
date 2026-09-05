8340c72cb96a2d92bdf6531f0851cb00e3c814a4
owaies <owaies786@gmail.com>
2026-09-05 23:40:05 +0530

    fix: point team photo loader at current GitHub assets

--- app/team-photo-loader.tsx
+++ app/team-photo-loader.tsx
@@ -1,17 +1,8 @@
 'use client';
 
-import { useEffect } from 'react';
-
-const OWAIES_PHOTO = 'https://raw.githubusercontent.com/owaies/MakeWebb/main/app/file_000000002d308211b0a2203107625baf.png';
-const AFAF_PHOTO = 'https://raw.githubusercontent.com/owaies/MakeWebb/main/app/IMG-20260905-WA0013.jpg';
+const OWAIES_PHOTO = '/team/owaies.jpg';
+const AFAF_PHOTO = '/team/file_000000001c20821185b00fb24f8c0312.png';
 
 export default function TeamPhotoLoader() {
-  useEffect(() => {
-    document.querySelectorAll<HTMLImageElement>('.person-image').forEach((image) => {
-      if (image.alt === 'Mohammed Owaies') image.src = OWAIES_PHOTO;
-      if (image.alt === 'Mohammed Afaf Hassan') image.src = AFAF_PHOTO;
-    });
-  }, []);
-
   return null;
 }
