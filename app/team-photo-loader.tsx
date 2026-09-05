e7f51ef3294a1608e3d40c527002cc11d3a406dd
owaies <owaies786@gmail.com>
2026-09-05 23:20:56 +0530

    fix: use uploaded GitHub team photos

--- app/team-photo-loader.tsx
+++ app/team-photo-loader.tsx
@@ -2,11 +2,14 @@
 
 import { useEffect } from 'react';
 
+const OWAIES_PHOTO = 'https://raw.githubusercontent.com/owaies/MakeWebb/main/app/file_000000002d308211b0a2203107625baf.png';
+const AFAF_PHOTO = 'https://raw.githubusercontent.com/owaies/MakeWebb/main/app/IMG-20260905-WA0013.jpg';
+
 export default function TeamPhotoLoader() {
   useEffect(() => {
     document.querySelectorAll<HTMLImageElement>('.person-image').forEach((image) => {
-      if (image.alt === 'Mohammed Owaies') image.src = '/team/owaies.jpg';
-      if (image.alt === 'Mohammed Afaf Hassan') image.src = '/team/afaf.jpg';
+      if (image.alt === 'Mohammed Owaies') image.src = OWAIES_PHOTO;
+      if (image.alt === 'Mohammed Afaf Hassan') image.src = AFAF_PHOTO;
     });
   }, []);
 
