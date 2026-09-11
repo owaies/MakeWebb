08894a906052aef1e1bb4a0b51becf0e1e9f6afc
owaies <owaies786@gmail.com>
2026-09-11 19:03:56 +0530

    Load responsive stability fixes after visual styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ReferenceMatch from './ReferenceMatch';
 import './index.css';
+import './responsive-fixes.css';
 
-createRoot(document.getElementById('root')!).render(<React.StrictMode><ReferenceMatch /></React.StrictMode>);
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode><ReferenceMatch /></React.StrictMode>
+);
