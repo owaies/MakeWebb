5c3fbddffdc4a7c5b1b7afe1aaac368066ed8357
owaies <owaies786@gmail.com>
2026-09-11 18:41:57 +0530

    Switch app entry to reference-matched experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import ReferenceExperience from './components/ReferenceExperience';
+import ReferenceMatch from './ReferenceMatch';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ReferenceExperience />
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')!).render(<React.StrictMode><ReferenceMatch /></React.StrictMode>);
