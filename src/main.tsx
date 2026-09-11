6812da263024e63218ff9df373b33ec94903e3e0
owaies <owaies786@gmail.com>
2026-09-11 17:57:55 +0530

    Use reference rebuild as app entry

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import ReferenceRebuild from './ReferenceRebuild';
 import './index.css';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+createRoot(document.getElementById('root')!).render(<React.StrictMode><ReferenceRebuild /></React.StrictMode>);
