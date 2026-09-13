4d52f9a0ed65122fb19cacfa9058297ece2a254c
owaies <owaies786@gmail.com>
2026-09-13 13:54:58 +0530

    Load cinematic choreography styles last

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './index.css';
 import './responsive-fixes.css';
 import './makewebb-reference.css';
 import './makewebb-exact.css';
+import './makewebb-cinematic.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>
