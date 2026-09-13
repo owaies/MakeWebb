79148acb9f17e49fc185e1a2fc970255d5cd278c
owaies <owaies786@gmail.com>
2026-09-14 00:00:02 +0530

    Remove legacy visual layers from MakeWebb entry

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
-import './responsive-fixes.css';
-import './makewebb-reference.css';
-import './makewebb-exact.css';
-import './makewebb-cinematic.css';
-import './makewebb-cinematic-scene.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>
